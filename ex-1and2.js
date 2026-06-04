c653e3b4ec0b1d5403e7da399379a6cd30bc2660
watcharawej <watcharawejsukyu@gmail.com>
2026-06-04 13:43:11 +0700

    feat: add new codes

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let secondName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const foodNetPrice = 15000;
+const restaurantNum = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+foodNetPrice = 5000;
+restaurantNum = 2500;
